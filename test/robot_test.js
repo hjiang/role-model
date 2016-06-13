a4887fc3986dbb81fc2b203ebd8b88d7af37bbab
Hong Jiang <hjiang@leancloud.rocks>
2016-06-13 19:17:07 +0800

    Unify handler interface

--- test/robot_test.js
+++ test/robot_test.js
@@ -14,7 +14,7 @@ describe('Robot', function() {
     it('should call handler if predicate is true', function() {
       let isRun = false;
       const bot = new robot.Robot();
-      bot.addPredHandler(() => true, () => isRun = true);
+      bot.addHandler(() => true, () => isRun = true);
       bot.handleRequest({});
       expect(isRun).to.equal(true);
     });
@@ -22,7 +22,7 @@ describe('Robot', function() {
     it('should not call handler if predicate is true', function() {
       let isRun = false;
       const bot = new robot.Robot();
-      bot.addPredHandler(() => false, () => isRun = true);
+      bot.addHandler(() => false, () => isRun = true);
       bot.handleRequest({});
       expect(isRun).to.equal(false);
     });
@@ -31,8 +31,8 @@ describe('Robot', function() {
       let isFirstRun = false;
       let isSecondRun = false;
       const bot = new robot.Robot();
-      bot.addPredHandler(() => true, () => isFirstRun = true);
-      bot.addPredHandler(() => true, () => isSecondRun = true);
+      bot.addHandler(() => true, () => isFirstRun = true);
+      bot.addHandler(() => true, () => isSecondRun = true);
       bot.handleRequest({});
       expect(isFirstRun).to.equal(true);
       expect(isSecondRun).to.equal(false);
@@ -43,7 +43,7 @@ describe('Robot', function() {
     it('should run if keywords match', function() {
       const bot = new robot.Robot();
       let isRun = false;
-      bot.addKeywordHandler(['foo', 'bar'], () => isRun = true);
+      bot.addHandler(['foo', 'bar'], () => isRun = true);
       bot.handleRequest({text: 'foo is not bar'});
       expect(isRun).to.equal(true);
     });
@@ -51,7 +51,7 @@ describe('Robot', function() {
     it('should not run if any keyword do not match', function() {
       const bot = new robot.Robot();
       let isRun = false;
-      bot.addKeywordHandler(['foo', 'bar'], () => isRun = true);
+      bot.addHandler(['foo', 'bar'], () => isRun = true);
       bot.handleRequest({text: 'foo is not br'});
       expect(isRun).to.equal(false);
     });
@@ -61,7 +61,7 @@ describe('Robot', function() {
     it('should run if the regex match', function() {
       const bot = new robot.Robot();
       let isRun = false;
-      bot.addRegexHandler(/fo*/, () => isRun = true);
+      bot.addHandler(/fo*/, () => isRun = true);
       bot.handleRequest({text: 'foo is not bar'});
       expect(isRun).to.equal(true);
     });
@@ -69,7 +69,7 @@ describe('Robot', function() {
     it('should not run if the regex does not match', function() {
       const bot = new robot.Robot();
       let isRun = false;
-      bot.addRegexHandler(/fu+/, () => isRun = true);
+      bot.addHandler(/fu+/, () => isRun = true);
       bot.handleRequest({text: 'foo is not bar'});
       expect(isRun).to.equal(false);
     });
@@ -77,7 +77,7 @@ describe('Robot', function() {
     it('should put matches into the context', function() {
       const bot = new robot.Robot();
       let isRun = false;
-      bot.addRegexHandler(/fo*/, (ctx) => {
+      bot.addHandler(/fo*/, (ctx) => {
         isRun = true;
         expect(ctx).to.have.property('matches').with.length(1);
         expect(ctx.matches[0]).to.equal('foo');
