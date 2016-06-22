b4e8a4e0150b45581f4108bd61d1d8efec532756
Hong Jiang <hjiang@leancloud.rocks>
2016-06-22 12:01:58 +0800

    Upgrade eslint-plugin-node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,14 @@
 module.exports = {
-  "extends": "airbnb",
+  extends: ['airbnb', 'plugin:node/recommended'],
   "installedESLint": true,
-  "plugins": [
-    "node"
+  plugins: [
+    'node',
   ],
-  "env": {
-    "es6": true,
-    "node": true
-
+  env: {
+    es6: true,
+    node: true,
   },
-  "rules": {
-    "no-console": 0,
-    "node/no-deprecated-api": "error",
-    "node/no-missing-import": "error",
-    "node/no-missing-require": "error",
-    "node/no-unpublished-import": "error",
-    "node/no-unpublished-require": "error",
-    "node/no-unsupported-features": ["error", {"version": 4}],
-    "node/process-exit-as-throw": "error",
-    "node/shebang": "error"
-  }
+  rules: {
+    'no-console': 0,
+  },
 };
